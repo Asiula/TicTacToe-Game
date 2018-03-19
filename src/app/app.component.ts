9f7119bd9c84366f18fe7416d2b58f43c007c74c
Asiula <fire.red.zelek@gmail.com>
2018-03-19 22:24:59 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    player1_name="Player 1";
+    getP1Name(p1_name: NgForm){
+        let name=p1_name.value;
+        let temp = name.player1;
+        this.player1_name= temp;
+    }
+    player2_name="Player 2";
+    getP2Name(p2_name: NgForm){
+        let name=p2_name.value;
+        let temp = name.player2;
+        this.player2_name= temp;
+             console.log(this.board);
+    }
+
+    //game logic
+    board:Array<any> = ["X","","",
+                        "","X","",
+                        "","",'X'];
+
+
 }
